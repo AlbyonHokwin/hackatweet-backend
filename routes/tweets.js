51cf374b659bf48c6509d01f5088e4134c9f4b79
Camille HAUSTANT <camillehaustant@gmail.com>
2022-11-24 14:27:39 +0100

    route GET for hashtags and last tweets done

--- routes/tweets.js
+++ routes/tweets.js
@@ -5,7 +5,7 @@ const Tweet = require('../models/tweets');
 const { checkBody } = require('../modules/checkBody');
 
 // POST: allow to create a new message to DB
-// Response: result
+// Response: result, tweet
 router.post('/new', async (req, res) => {
     if (checkBody(req.body, ['message', 'token'])) {
         const { message, token } = req.body;
@@ -50,16 +50,23 @@ router.delete('/delete', async (req, res) => {
     } else res.json({ result: false, error: 'Missong or empty fields' });
 });
 
-// GET: take the last 20 tweets
+// GET: take the last
+const numOfLast = 20; // tweets
 // Response: result, tweets (tweets sorted by date from more recent)
-router.get('/lasts', (req, res) => {
-    res.json({});
+router.get('/lasts', async (req, res) => {
+    const tweets = await Tweet.find().sort({ date: -1 });
+    tweets[0] ?
+        res.json({ result: true, tweets: tweets.slice(0, numOfLast) }) :
+        res.json({ result: false, error: 'No Tweet' });
 });
 
 // GET: Find tweets with the hashtag in parameter
 // Response: result, tweets (tweets sorted by date from more recent)
-router.get('/:hashtag', (req, res) => {
-    res.json({});
+router.get('/hashtag/:hashtag', async (req, res) => {
+    const tweets = await Tweet.find({ hashtags: { $in: req.params.hashtag } });
+    tweets[0] ?
+        res.json({ result: true, tweets }) :
+        res.json({ result: false, error: 'No tweet for this hashtag' });
 });
 
 // GET: Find the 10 first hashtags with the more tweets
